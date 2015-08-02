75e969de63e5c32da407489f33f398a5a2a46043
Dave Schumaker <daveschumaker@gmail.com>
2015-08-02 16:59:07 -0700

    Pull out Jade templating and remove unneeded modules.

--- server.js
+++ server.js
@@ -4,60 +4,27 @@
  */
 
 var express = require('express');
-var stylus = require('stylus');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
+var flags = require('./server/world-flags');
 // Setup default environment
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-
-// Configure Stylus module for compiling CSS
-function compile (str, path) {
-  return stylus(str).set('filename', path);
-}
-
-// Configure views engine using Jade
-app.set('views', __dirname + '/server/views');
-app.set('view engine', 'jade');
 app.use(morgan('dev'));
-app.use(bodyParser());
-app.use(stylus.middleware({
-  src: __dirname + '/public',
-  compile: compile
-}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/flags');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error!'));
-db.once('open', function callback() {
-  console.log('Database connection to flags is open...');
-})
+// mongoose.connect('mongodb://localhost/flags');
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error!'));
+// db.once('open', function callback() {
+//   console.log('Database connection to flags is open...');
+// })
 
-var messageSchema = mongoose.Schema({
-  message: String
-})
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-  mongoMessage = messageDoc.message;
-});
-
-app.get('/partials/:partialPath', function(req, res) {
-  res.render('partials/' + req.params.partialPath);
-});
-
-// Catch all route handler.
-// TODO: Build out specific routes that the server is aware of.
-app.get('*', function(req, res) {
-  res.render('index', {
-    mongoMessage: mongoMessage,
-  });
-})
+console.log(flags.getRandom());
 
 // Initialize server
 var port = 3000;
