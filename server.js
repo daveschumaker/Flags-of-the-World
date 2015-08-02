a1b8d06d41f84337e6e397d052dea5b8dfb6c5bc
Dave Schumaker <daveschumaker@gmail.com>
2015-08-02 14:59:34 -0700

    Add morgan, stylus, and bodyParser modules to project.

--- server.js
+++ server.js
@@ -4,15 +4,31 @@
  */
 
 var express = require('express');
+var stylus = require('stylus');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 // Setup default environment
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-// Configure views engine
+
+// Configure Stylus module for compiling CSS
+function compile (str, path) {
+  return stylus(str).set('filename', path);
+}
+
+// Configure views engine using Jade
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compile
+}))
+app.use(express.static(__dirname + '/public'));
 
 // Catch all route handler.
 // TODO: Build out specific routes that the server is aware of.
